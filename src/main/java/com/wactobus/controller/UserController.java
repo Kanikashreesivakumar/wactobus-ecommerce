da857152136d3aa73c198519d1f258dc3f893c4e
kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2025-06-03 15:04:38 +0530

    added changes

--- src/main/java/com/wactobus/controller/UserController.java
+++ src/main/java/com/wactobus/controller/UserController.java
@@ -1,31 +1,32 @@
 package com.wactobus.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.wactobus.model.User;
 import com.wactobus.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     
     @Autowired
     private UserService userService;
     
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        return ResponseEntity.ok(userService.createUser(user));
-    }
-    
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
+        try {
+            logger.info("Attempting to create user: {}", user.getEmail());
+            User createdUser = userService.createUser(user);
+            logger.info("Successfully created user with ID: {}", createdUser.getId());
+            return ResponseEntity.ok(createdUser);
+        } catch (Exception e) {
+            logger.error("Error creating user: ", e);
+            throw new RuntimeException("Error creating user: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/wactobus/model/User.java
+++ src/main/java/com/wactobus/model/User.java
@@ -1,41 +1,25 @@
-package com.wactobus.model;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+package com.wactobus.model;import jakarta.persistence.*;@Entity@Table(name = "users")public class User {    @Id    @GeneratedValue(strategy = GenerationType.IDENTITY)    private Long id;        @Column(unique = true, nullable = false)    private String email;        private String firstName;    private String lastName;        // Getters and Setters    public Long getId() {         return id;     }        public void setId(Long id) {         this.id = id;     }    
+    public String getEmail() { 
+        return email; 
+    }
     
-    @Column(unique = true, nullable = false)
-    private String email;
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
     
-    @Column(nullable = false)
-    private String password;
+    public String getFirstName() { 
+        return firstName; 
+    }
     
-    private String firstName;
-    private String lastName;
+    public void setFirstName(String firstName) { 
+        this.firstName = firstName; 
+    }
     
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public String getLastName() { 
+        return lastName; 
+    }
     
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setLastName(String lastName) { 
+        this.lastName = lastName; 
+    }
 }
